95ee6417293b19a05986085b80b149e8676f86ba
Veronika Zwickenpflug <zwickenp@hm.edu>
2019-05-28 23:11:57 +0200

    Add JavaContainer test

--- src/test/java/io/jenkins/plugins/analysis/warnings/DockerITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/DockerITest.java
@@ -9,6 +9,7 @@ import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.test.acceptance.docker.DockerContainer;
 import org.jenkinsci.test.acceptance.docker.DockerRule;
+import org.jenkinsci.test.acceptance.docker.fixtures.JavaContainer;
 import hudson.model.Result;
 import hudson.plugins.sshslaves.SSHLauncher;
 import hudson.slaves.DumbSlave;
@@ -28,8 +29,50 @@ import static org.assertj.core.api.Assertions.*;
 public class DockerITest extends IntegrationTestWithJenkinsPerSuite {
 
     @Rule
-    public DockerRule<GccMakeContainer> gccDockerRule = new DockerRule(GccMakeContainer.class);
+    public DockerRule<GccMakeContainer> gccDockerRule = new DockerRule<>(GccMakeContainer.class);
 
+    @Rule
+    public DockerRule<JavaContainer> javaDockerRule = new DockerRule<>(JavaContainer.class);
+
+    /**
+     * Checks if the Analysis result is correctly, when running inside a java container.
+     *
+     * @throws Exception
+     *         When slave creation or waiting for node start fails.
+     */
+    @Test
+    public void checkRunningInJavaContainerDocker() throws Exception {
+        DumbSlave agent = createAgent(javaDockerRule.get());
+        WorkflowJob job = createPipeline();
+        job.setDefinition(new CpsFlowDefinition("pipeline {\n"
+                + "    agent { label 'docker' }\n"
+                + "    stages {\n"
+                + "        stage ('Record Issues') {\n"
+                + "            steps {\n"
+                + "                recordIssues(tools: [taskScanner(highTags: 'FIXMEE', normalTags: 'TODOO', includePattern: '**/*.java')])\n"
+                + "            }\n"
+                + "        }\n"
+                + "    }\n"
+                + "}", true));
+
+        copySingleFileToAgentWorkspace(agent, job, "docker/Main.java", "Main.java");
+
+        AnalysisResult result = scheduleBuildAndAssertStatus(job, Result.SUCCESS);
+
+        assertThat(result.getErrorMessages()).isEmpty();
+        assertThat(result.getInfoMessages()).contains(
+                "Found a total of 2 open tasks",
+                "-> FIXMEE: 1 open tasks",
+                "-> TODOO: 1 open tasks"
+        );
+    }
+
+    /**
+     * Checks if the Analysis result is correctly, when running inside a gcc make container.
+     *
+     * @throws Exception
+     *         When slave creation or waiting for node start fails.
+     */
     @Test
     public void shouldRecordWithGccAndMake() throws Exception {
         DumbSlave agent = createAgent(gccDockerRule.get());
@@ -50,6 +93,7 @@ public class DockerITest extends IntegrationTestWithJenkinsPerSuite {
                 + "        }\n"
                 + "    }\n"
                 + "}", true));
+
         copySingleFileToAgentWorkspace(agent, job, "docker/gcc/main.cpp", "main.cpp");
         copySingleFileToAgentWorkspace(agent, job, "docker/gcc/makefile", "makefile");
 
